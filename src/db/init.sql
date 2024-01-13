1e99841030486fa7178c153fdacd74050725b9b8
akatranlp <fabian@nf-petersen.de>
2024-01-13 09:33:37 +0100

    feat(db): Add and/or change test data

--- src/db/init.sql
+++ src/db/init.sql
@@ -45,7 +45,7 @@ create table if not exists transactions
 
 insert into users (email, password, profile_name, balance)
 values ('test@test.com', '<bcrypt password hash 1>', 'Toni Tester', 1000),
-       ('max.mustermann@gmail.com', '<bcrypt password hash 2>', 'Max Mustermann', 1000);
+       ('test', '<bcrypt password hash 1>', 'Test User', 1000);
 
 insert into books (name, authorId, description)
 values ('Book One', 1, 'A good book'),
@@ -56,14 +56,18 @@ insert into chapters (id, bookId, name, price, content, status)
 values (1, 1, 'The beginning', 0, 'Lorem Ipsum', 1),
        (2, 1,'The beginning 2: Electric Boogaloo', 100, 'Lorem Ipsum 2', 1),
        (3, 1, 'The beginning 3: My Enemy', 100, 'Lorem Ipsum 3', 1),
-       (1, 2, 'A different book chapter 1', 0, 'LorIp 4', 1),
+       (1, 2, 'A different book chapter 1', 0, 'Lorem Ipsum 4', 1),
        (2, 2, 'What came after', 100, 'Lorem Ipsum 5', 1),
-	   (3, 2, 'FFFFFFFFFFFFFFFF', 100, 'Lorem Ipsum 5', 1),
-	   (4, 2, 'VVVVVVVVVVVVVVVVV', 100, 'Lorem Ipsum 5', 1),
-	   (5, 2, 'DSDDDDDDDDDDDDDDDD', 100, 'Lorem Ipsum 5', 1),
-	   (6, 2, 'TTTTTTTTTTTTT', 100, 'Lorem Ipsum 5', 1);
+	   (3, 2, 'What came after that', 500, 'Lorem Ipsum 6', 1),
+	   (4, 2, 'And there after ', 400, 'Lorem Ipsum 7', 1),
+	   (1, 3, 'The third book chapter 1', 750, 'Lorem Ipsum 8', 1),
+	   (2, 3, 'The third book chapter 2', 800, 'Lorem Ipsum 9', 1),
+	   (3, 3, 'The third book chapter 3', 900, 'Lorem Ipsum 10', 1);
 
 insert into transactions (bookid, chapterid, receivinguserid, payinguserid, amount)
 values (1, 1, 1, 2, 0),
        (1, 2, 1, 2, 100),
-       (2, 1, 2, 1, 0);
+       (2, 1, 2, 1, 0),
+	   (2, 4, 2, 1, 400),
+	   (3, 1, 1, 2, 750),
+	   (3, 2, 1, 2, 800);
